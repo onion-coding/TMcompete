05b128d799d3e3148e6fa254a0c071e6f575e999
alexander polzin <alexander@girls.io>
2025-09-21 21:13:42 +0200

    Update competition.py

--- resources/competition.py
+++ resources/competition.py
@@ -2,12 +2,13 @@
 
 from db.models import Competition
 
-class TestResource:
+class CompetitionResource:
+    # list competitions
     def on_get(self, req, resp):
         session = req.context.session  # <-- use context
         competitions = session.query(Competition).all()
         resp.media = [{"id": c.id, "title": c.title} for c in competitions]
 
-    # implements event creation
+    # create competition 
     def on_post(self, req, resp):
         return
